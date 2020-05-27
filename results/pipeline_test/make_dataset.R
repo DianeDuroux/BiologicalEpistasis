fe2977413af7eb69cf4b8334b29ca23a6f55ef7a
hclimente <hclimente@gmail.com>
2020-05-27 15:36:19 +0200

    added benchmark files

---
+++ results/pipeline_test/make_dataset.R
@@ -0,0 +1,24 @@
+library(tidyverse)
+
+eligible_snps <- read_tsv('CD_UC_CON_QCed_rel1_without_relatives_maf0.05_hwe0.001_Liu2015_232SNPs.bim', col_names = F) %>% filter(X1 != 6) %>% .$X2
+pos <- read_csv('~/projects/BiologicalEpistasis/results/withBiofilter_filter/eqtl/sign_SNPpairs.txt')
+pos <- unique(c(pos$SNP_1, pos$SNP_2))
+
+eqtl <- read_tsv('eqtl_mapping.tsv')
+
+other_snps <- filter(eqtl, (rsID %in% eligible_snps) & (!rsID %in% pos)) %>%
+    .$rsID %>%
+    unique %>%
+    sample(500)
+snps = c(pos, other_snps) 
+
+tibble(snp = snps) %>%
+    write_tsv('synthetic_snps.txt')
+
+eqtl <- read_tsv('eqtl_mapping.tsv') %>% filter(rsID %in% snps)
+info <- read_tsv('genes.tsv') %>% select(ensg, HUGO)
+
+inner_join(eqtl, info) %>% select(rsID, HUGO, tissue) %>% rename(snp = rsID, gene = HUGO) %>% write_tsv('eqtl_benchmark.tsv')
+
+genes <- read_tsv('eqtl_benchmark.tsv')$gene
+read_tsv('biofilter.gene.models') %>% filter(`#gene1` %in% genes & gene2 %in% genes)  %>% rename(`Official Symbol Interactor A` = `#gene1`, `Official Symbol Interactor B` = gene2) %>% write_tsv('models.tab2')
